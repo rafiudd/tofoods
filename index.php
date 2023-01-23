d6af272ec46be16cad77a58964e7b814ec85cfa5
rafiudd <rayhanrafiudd@gmail.com>
2023-01-23 22:57:49 +0700

    Fix: Value Id onClick Cart

--- index.php
+++ index.php
@@ -407,8 +407,8 @@
                     <?php
                         // session_start();
                         if(isset($_SESSION['name'])) {
-                            echo ' <form id="my_form" action="cart-action.php?id=' . $baris_data['id'] . '" method="POST">
-                            <a href="#" onclick="showAlert()">
+                          echo ' <form id="my_form" method="POST">
+                          <a href="cart-action.php?id='. $baris_data["id"] .'" onclick="showAlert()">
                                   <svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 456.029 456.029" style="enable-background:new 0 0 456.029 456.029;" xml:space="preserve">
                                     <g>
                                       <g>
